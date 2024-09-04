06da0593ead7bb3f6a7bd33b09ac12db24e824ae
sasaplus1 <sasaplus1@gmail.com>
2024-09-04 23:41:44 +0900

    guess

--- chapter2/gussing_game/src/main.rs
+++ chapter2/gussing_game/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    // std::io::stdin()
+    io::stdin()
+        // &guessでなく&mut guessなので参照を可変とする
+        .read_line(&mut guess)
+        // io::Resultはexpectを持っておりプログラムをクラッシュできる
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
