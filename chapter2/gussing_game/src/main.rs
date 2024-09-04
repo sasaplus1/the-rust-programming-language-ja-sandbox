7356a6a7c6b7b2ebbf88b8aca49eab115fdfc7a9
sasaplus1 <sasaplus1@gmail.com>
2024-09-04 23:58:38 +0900

    cmp and parse

--- chapter2/gussing_game/src/main.rs
+++ chapter2/gussing_game/src/main.rs
@@ -1,7 +1,17 @@
+// $ cargo doc --open で使用しているクレートのドキュメントを見ることができる
+
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    // 1..101は1以上101未満の範囲を表す
+    // 1..=100という範囲を渡すこともできる
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -14,5 +24,15 @@ fn main() {
         // io::Resultはexpectを持っておりプログラムをクラッシュできる
         .expect("Failed to read line");
 
+    // 同じ名前で変数を宣言することでシャドーイングできる
+    // parse()のために変数の型を明示的に指定する必要がある
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
